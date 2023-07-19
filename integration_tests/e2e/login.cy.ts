f82af252ca9539b215e941a3df0cc79e2afd8400
David Middleton <david.middleton@digital.justice.gov.uk>
2023-07-19 11:59:51 +0100

    Fix login.cy.ts fail

--- integration_tests/e2e/login.cy.ts
+++ integration_tests/e2e/login.cy.ts
@@ -46,6 +46,7 @@ context('SignIn', () => {
 
   it('Token verification failure takes user to sign in page', () => {
     cy.signIn()
+    cy.setupUserAuth()
     Page.verifyOnPage(IndexPage)
     cy.task('stubVerifyToken', false)
 
@@ -55,6 +56,7 @@ context('SignIn', () => {
 
   it('Token verification failure clears user session', () => {
     cy.signIn()
+    cy.setupUserAuth()
     const indexPage = Page.verifyOnPage(IndexPage)
     cy.task('stubVerifyToken', false)
 
@@ -62,7 +64,7 @@ context('SignIn', () => {
     cy.request('/').its('body').should('contain', 'Sign in')
 
     cy.task('stubVerifyToken', true)
-    cy.task('stubAuthUser', 'bobby brown')
+    cy.task('stubAuthUser', { name: 'bobby brown' })
     cy.signIn()
 
     indexPage.headerUserName().contains('B. Brown')
