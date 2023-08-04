57b844ef04392ff5f202cc2b011d041d9c7fbf48
jamesgrant-moj <116157538+jamesgrant-moj@users.noreply.github.com>
2023-08-04 09:57:40 +0100

    CDPS-262 dps services store update (#17)
    * CDPS-262 - update to single service store
    
    * CDPS-262 - update to single service store
    
    * CDPS-262 - remove question marks
    
    * CDPS-262 - update for linting

--- server/data/dpsServicesDataStore.test.ts
+++ server/data/dpsServicesDataStore.test.ts
@@ -1,11 +1,11 @@
 import { Role } from '../enums/role'
-import dpsServicesDataStoreMock from '../mocks/dpsServicesDataStoreMock'
+import { dpsServicesDataStoreMockA } from '../mocks/dpsServicesDataStoreMock'
 import { getTasks } from './dpsServicesDataStore'
 
 describe('DPS Services Data Store', () => {
   it('Should return all the dps services data', () => {
-    expect(getTasks('MDI', [], '123', { enabled: false }, { migrated: false }, [Role.GlobalSearch])).toEqual(
-      dpsServicesDataStoreMock,
+    expect(getTasks('LEI', [], '123', { enabled: false }, { migrated: false }, [Role.GlobalSearch])).toEqual(
+      dpsServicesDataStoreMockA,
     )
   })
 })
