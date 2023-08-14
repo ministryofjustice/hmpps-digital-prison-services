049adb91f36dbb89392d511ac26ddbd04182603e
David Middleton <david.middleton@digital.justice.gov.uk>
2023-08-14 19:11:55 +0100

    Fix lint errors

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
+import path from 'path'
 import nunjucks from 'nunjucks'
 import express from 'express'
-import path from 'path'
 import {
   addDefaultSelectedValue,
   asSelectItem,
