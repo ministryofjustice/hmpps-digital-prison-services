fab2c1b883d41af83baf28a341678a049466658e
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2023-09-08 16:14:03 +0100

    request front end components with a single call

--- server/middleware/frontEndComponents.ts
+++ server/middleware/frontEndComponents.ts
@@ -6,10 +6,8 @@ import { Services } from '../services'
 export default function getFrontendComponents({ componentService }: Services): RequestHandler {
   return async (req, res, next) => {
     try {
-      const [header, footer] = await Promise.all([
-        componentService.getComponent('header', res.locals.user.token),
-        componentService.getComponent('footer', res.locals.user.token),
-      ])
+      const { header, footer } = await componentService.getComponents(['header', 'footer'], res.locals.user.token)
+
       res.locals.feComponents = {
         header: header.html,
         footer: footer.html,
--- server/services/componentService.ts
+++ server/services/componentService.ts
@@ -1,11 +1,15 @@
 import { RestClientBuilder } from '../data'
 import { ComponentApiClient } from '../data/interfaces/componentApiClient'
 import Component from '../data/interfaces/component'
+import AvailableComponent from '../data/interfaces/AvailableComponent'
 
 export default class ComponentService {
   constructor(private readonly componentApiClientBuilder: RestClientBuilder<ComponentApiClient>) {}
 
-  public async getComponent(component: 'header' | 'footer', userToken: string): Promise<Component> {
-    return this.componentApiClientBuilder(userToken).getComponent(component, userToken)
+  async getComponents<T extends AvailableComponent[]>(
+    components: T,
+    userToken: string,
+  ): Promise<Record<T[number], Component>> {
+    return this.componentApiClientBuilder(userToken).getComponents(components, userToken)
   }
 }
