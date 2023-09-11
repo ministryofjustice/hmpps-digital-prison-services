166c749ff6e0b688c44ddfecdead0f46c6437644
whitfield-mj <matthew.whitfield@digital.justice.gov.uk>
2023-09-11 09:29:05 +0100

    request front end components with a single call (#32)

--- server/data/interfaces/componentApiClient.ts
+++ server/data/interfaces/componentApiClient.ts
@@ -1,5 +1,6 @@
 import Component from './component'
+import AvailableComponent from './AvailableComponent'
 
 export interface ComponentApiClient {
-  getComponent(component: 'header' | 'footer', userToken: string): Promise<Component>
+  getComponents<T extends AvailableComponent[]>(component: T, userToken: string): Promise<Record<T[number], Component>>
 }
