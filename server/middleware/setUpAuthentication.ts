00fb0b761c4d3551fec421dbccbbc9b6136958d5
Dani Burnley <dan@danops.uk>
2023-12-04 14:52:14 +0000

    Implement auth handler from dps core

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -1,4 +1,4 @@
-import type { ErrorRequestHandler, RequestHandler, Router } from 'express'
+import type { Router } from 'express'
 import express from 'express'
 import passport from 'passport'
 import flash from 'connect-flash'
@@ -22,23 +22,35 @@ export default function setUpAuth(): Router {
 
   router.get('/sign-in', passport.authenticate('oauth2'))
 
-  const authSuccess: RequestHandler = (req, res) => {
-    const redirectUrl = req.session.returnTo || '/'
-    res.redirect(redirectUrl)
-  }
-  const authFailure: ErrorRequestHandler = (err, req, res, next) => {
-    logger.error(`Passport auth error: ${err}`)
-    req.session.destroy(() => res.redirect('/autherror'))
-  }
-
-  router.get(
-    '/sign-in/callback',
-    passport.authenticate('oauth2', {
-      failWithError: true,
-    }),
-    authSuccess,
-    authFailure,
-  )
+  router.get('/sign-in/callback', (req, res, next) => {
+    const authCallback: passport.AuthenticateCallback = (err, user, info) => {
+      if (err) {
+        return res.redirect('/autherror')
+      }
+      if (!user) {
+        const { message } = info as { message: string }
+        if (info && message === 'Unable to verify authorization request state.') {
+          // failure to due authorisation state not being there on return, so retry
+          logger.info('Retrying auth callback as no state found')
+          return res.redirect('/')
+        }
+        logger.info(`Auth failure due to ${JSON.stringify(info)}`)
+        return res.redirect('/autherror')
+      }
+      const { returnTo } = req.session
+      req.logIn(user, err2 => {
+        if (err2) {
+          return next(err2)
+        }
+        if (typeof returnTo === 'string' && returnTo.startsWith('/')) {
+          return res.redirect(returnTo)
+        }
+        return res.redirect('/')
+      })
+      return null
+    }
+    passport.authenticate('oauth2', authCallback)(req, res, next)
+  })
 
   const authUrl = config.apis.hmppsAuth.externalUrl
   const authSignOutUrl = `${authUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
