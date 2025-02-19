7b502f5f7f5529e04cc6bb2ae6948f9e7c5fbf11
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-02-19 17:38:37 +0000

    MAP-1882: Removing e2e tests for establishment roll

--- integration_tests/pages/InReception.ts
+++
@@ -1,9 +0,0 @@
-import Page, { PageElement } from './page'
-
-export default class InReceptionPage extends Page {
-  constructor() {
-    super(`In reception`)
-  }
-
-  inReceptionRows = (): PageElement => cy.get('table.in-reception-roll__table tbody tr')
-}
--- integration_tests/pages/totalOut.ts
+++
@@ -1,9 +0,0 @@
-import Page, { PageElement } from './page'
-
-export default class TotalOutPage extends Page {
-  constructor() {
-    super('Total currently out')
-  }
-
-  currentlyOutRows = (): PageElement => cy.get('table.currently-out-roll__table tbody tr')
-}
