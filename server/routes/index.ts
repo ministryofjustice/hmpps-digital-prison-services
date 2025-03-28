d14277077c1456f909f9f0c19fd7a2b6561f63c1
Melia Burnley <dan@danops.uk>
2025-03-28 10:59:45 +0000

    CDPS-1319: Notification spike

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,7 +5,53 @@ import HomepageController from '../controllers/homepageController'
 import whatsNewRouter from './whatsNewRouter'
 import managedPageRouter from './managedPageRouter'
 import dietaryRequirementsRouter from './dietaryRequirementsRouter'
-import config from '../config'
+import config, { AgentConfig } from '../config'
+import RestClient from '../data/restClient'
+import restClientBuilder from '../data'
+
+class ComponentClient {
+  constructor(private restClient: RestClient) {}
+
+  private get<T>(args: object): Promise<T> {
+    return this.restClient.get<T>(args)
+  }
+
+  private async post<T>(args: object): Promise<T> {
+    return this.restClient.post<T>(args)
+  }
+
+  async addNotification(data: any, userToken: string) {
+    await this.restClient.post({
+      path: '/notifications/add-notification',
+      data,
+      headers: {
+        'x-user-token': userToken,
+      },
+    })
+  }
+  async seeNotification(data: any, userToken: string) {
+    await this.restClient.post({
+      path: '/notifications/see-notification',
+      data,
+      headers: {
+        'x-user-token': userToken,
+      },
+    })
+  }
+}
+
+const componentClientBuilder = restClientBuilder(
+  'Component',
+  {
+    url: 'http://localhost:3001',
+    timeout: {
+      response: 20000,
+      deadline: 20000,
+    },
+    agent: { timeout: 2000 },
+  },
+  ComponentClient,
+)
 
 export default function routes(services: Services): Router {
   const router = Router()
@@ -28,6 +74,37 @@ export default function routes(services: Services): Router {
 
   get('/', homepageController.displayHomepage())
   post('/search', homepageController.search())
+  get(
+    '/add-notification',
+    asyncMiddleware(async (req, res, next) => {
+      return res.render('pages/notifications/add', {
+        userId: res.locals.user.userId,
+      })
+    }),
+  )
+
+  get(
+    '/notification/:notificationId',
+    asyncMiddleware(async (req, res, next) => {
+      const { notificationId } = req.params
+      const url = req.query?.url as string
+      const { clientToken } = req.middleware
+      const client = componentClientBuilder(clientToken)
+      await client.seeNotification({ userId: res.locals.user.userId, notificationId }, res.locals.user.token)
+      return res.redirect(decodeURIComponent(url))
+    }),
+  )
+
+  post(
+    '/add-notification',
+    asyncMiddleware(async (req, res, next) => {
+      const { clientToken } = req.middleware
+      const client = componentClientBuilder(clientToken)
+      await client.addNotification(req.body, res.locals.user.token)
+      return res.redirect('/add-notification')
+    }),
+  )
+
   router.use(managedPageRouter(services))
   router.use('/whats-new', whatsNewRouter(services))
   router.use('/dietary-requirements', dietaryRequirementsRouter(services))
