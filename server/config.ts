ee49c8a0452219efcb10e501d55f9c457b5a4cef
Melia Burnley <melia.burnley@digital.justice.gov.uk>
2025-10-02 11:27:03 +0100

    CDPS-1173: Add audit to dietary report (#308)

--- server/config.ts
+++ server/config.ts
@@ -42,6 +42,9 @@ export default {
     secret: get('SESSION_SECRET', 'app-insecure-default-session', requiredInProduction),
     expiryMinutes: Number(get('WEB_SESSION_TIMEOUT_IN_MINUTES', 120)),
   },
+  audit: {
+    enabled: get('AUDIT_ENABLED', 'false') === 'true',
+  },
   apis: {
     hmppsAuth: {
       url: get('HMPPS_AUTH_URL', 'http://localhost:9090/auth', requiredInProduction),
--- server/test/mocks/healthAndMedicationApiClientMock.ts
+++ server/test/mocks/healthAndMedicationApiClientMock.ts
@@ -1,7 +1,73 @@
-import { HealthAndMedicationApiClient } from '../../data/interfaces/healthAndMedicationApiClient'
+import {
+  HealthAndMedicationApiClient,
+  HealthAndMedicationForPrison,
+} from '../../data/interfaces/healthAndMedicationApiClient'
+import { PagedList } from '../../data/interfaces/pagedList'
+
+export const mockHealthAndMedicationResponse = {
+  content: [
+    {
+      firstName: 'Richard',
+      lastName: 'Smith',
+      prisonerNumber: 'G4879UP',
+      location: 'C-3-010',
+      health: {
+        dietAndAllergy: {
+          medicalDietaryRequirements: {
+            value: [
+              { value: { id: 'x', description: 'Coeliac (cannot eat gluten)' } },
+              { value: { id: 'x', description: 'Nutrient Deficiency' } },
+              { value: { id: '_OTHER' }, comment: 'has to eat a low copper diet' },
+            ],
+          },
+          foodAllergies: { value: [{ value: { id: 'x', description: 'Egg' } }] },
+          personalisedDietaryRequirements: { value: [{ value: { id: 'x', description: 'Kosher' } }] },
+          cateringInstructions: { value: 'Serve food on a plate' },
+        },
+      },
+    },
+    {
+      firstName: 'George',
+      lastName: 'Harrison',
+      name: 'George Harrison',
+      prisonerNumber: 'G6333VK',
+      location: 'B-1-042',
+      health: {
+        dietAndAllergy: {
+          medicalDietaryRequirements: { value: [] },
+          foodAllergies: { value: [{ value: { id: 'x', description: 'Sesame' } }] },
+          personalisedDietaryRequirements: { value: [] },
+        },
+      },
+    },
+    {
+      firstName: 'Harry',
+      lastName: 'Thompson',
+      prisonerNumber: 'G3101UO',
+      location: 'F-5-031',
+      health: {
+        dietAndAllergy: {
+          medicalDietaryRequirements: { value: [] },
+          foodAllergies: { value: [] },
+          personalisedDietaryRequirements: { value: [{ value: { id: 'x', description: 'Kosher' } }] },
+        },
+      },
+    },
+  ],
+  metadata: {
+    first: true,
+    last: false,
+    numberOfElements: 3,
+    offset: 0,
+    pageNumber: 1,
+    size: 3,
+    totalElements: 3,
+    totalPages: 1,
+  },
+} as unknown as PagedList<HealthAndMedicationForPrison>
 
 const healthAndMedicationApiClientMock: HealthAndMedicationApiClient = {
-  getHealthAndMedicationForPrison: jest.fn(),
+  getHealthAndMedicationForPrison: jest.fn(async () => mockHealthAndMedicationResponse),
 }
 
 export default healthAndMedicationApiClientMock
