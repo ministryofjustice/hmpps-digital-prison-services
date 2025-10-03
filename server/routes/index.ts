6efe30b90228967b2347a42b2d3e24f6bd31a212
ushkarev <ushkarev@users.noreply.github.com>
2025-10-03 13:02:46 +0100

    CDPS-1394: Express 5 compatibility (#314)
    * Remove `asyncMiddleware` as it’s no longer needed with express 5
    * Remove indirection when adding `get`/`post` request handlers to a router – it is now unnecessary since there is no special async middleware and the router’s true methods accept more flexible arguments
    * Prevent (unlikely) error if `req.body` is undefined, which can happen with express 5

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,4 @@
-import { type RequestHandler, Router } from 'express'
-import asyncMiddleware from '../middleware/asyncMiddleware'
+import { Router } from 'express'
 import type { Services } from '../services'
 import HomepageController from '../controllers/homepageController'
 import whatsNewRouter from './whatsNewRouter'
@@ -9,16 +8,6 @@ import config from '../config'
 
 export default function routes(services: Services): Router {
   const router = Router()
-  const get = (path: string | string[], ...handlers: RequestHandler[]) =>
-    router.get(
-      path,
-      handlers.map(handler => asyncMiddleware(handler)),
-    )
-  const post = (path: string | string[], ...handlers: RequestHandler[]) =>
-    router.post(
-      path,
-      handlers.map(handler => asyncMiddleware(handler)),
-    )
 
   const homepageController = new HomepageController(
     services.contentfulService,
@@ -26,17 +15,14 @@ export default function routes(services: Services): Router {
     services.serviceData,
   )
 
-  get('/', homepageController.displayHomepage())
-  post('/search', homepageController.search())
+  router.get('/', homepageController.displayHomepage())
+  router.post('/search', homepageController.search())
   router.use(managedPageRouter(services))
   router.use('/whats-new', whatsNewRouter(services))
   router.use('/dietary-requirements', dietaryRequirementsRouter(services))
-  router.get(
-    '/establishment-roll{*path}',
-    asyncMiddleware(async (req, res) => {
-      res.render('pages/establishmentRollHasMoved', { establishmentRollUrl: config.apis.establishmentRoll.ui_url })
-    }),
-  )
+  router.get('/establishment-roll{*path}', (_req, res) => {
+    res.render('pages/establishmentRollHasMoved', { establishmentRollUrl: config.apis.establishmentRoll.ui_url })
+  })
 
   return router
 }
--- server/routes/managedPageRouter.ts
+++ server/routes/managedPageRouter.ts
@@ -1,23 +1,16 @@
-import { RequestHandler, Router } from 'express'
+import { Router } from 'express'
 import { Services } from '../services'
-import asyncMiddleware from '../middleware/asyncMiddleware'
 import ManagedPageController from '../controllers/managedPageController'
 
 export default function whatsNewRouter(services: Services): Router {
   const router = Router()
 
-  const get = (path: string | string[], ...handlers: RequestHandler[]) =>
-    router.get(
-      path,
-      handlers.map(handler => asyncMiddleware(handler)),
-    )
-
   const managedPageController = new ManagedPageController(services.contentfulService)
 
-  get('/accessibility-statement', managedPageController.displayManagedPage('accessibility-statement'))
-  get('/privacy-policy', managedPageController.displayManagedPage('privacy-policy'))
-  get('/terms-and-conditions', managedPageController.displayManagedPage('terms-and-conditions'))
-  get('/cookies-policy', managedPageController.displayManagedPage('cookies-policy'))
+  router.get('/accessibility-statement', managedPageController.displayManagedPage('accessibility-statement'))
+  router.get('/privacy-policy', managedPageController.displayManagedPage('privacy-policy'))
+  router.get('/terms-and-conditions', managedPageController.displayManagedPage('terms-and-conditions'))
+  router.get('/cookies-policy', managedPageController.displayManagedPage('cookies-policy'))
 
   router.use('/learn-more-about-dps', managedPageController.displayManagedPage('learn-more-about-dps'))
 
