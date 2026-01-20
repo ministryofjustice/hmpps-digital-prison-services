4666b971534c966b6a150cd88e76252e01a8691f
Melia Burnley <melia.burnley@justice.gov.uk>
2026-01-20 12:07:30 +0000

    CDPS-1727: Audit local and global search results

--- server/services/auditService.ts
+++ server/services/auditService.ts
@@ -1,5 +1,44 @@
 import { auditService } from '@ministryofjustice/hmpps-audit-client'
 
+export interface PrisonerSearchDetails {
+  query: {
+    term?: string
+    alerts?: string[]
+    location?: string
+    view?: string
+    sort?: string
+    page?: number
+    size?: number
+    showAll?: boolean
+  }
+  results: {
+    prisonerNumbers: string[]
+    prisonerInformationDisplayed: string[]
+  }
+}
+
+export interface GlobalSearchDetails {
+  query: {
+    page: number
+    searchText: string
+    location: string
+    gender: string
+    referrer: string
+    dateOfBirth: {
+      day: string
+      month: string
+      year: string
+    }
+  }
+  results: {
+    prisonerNumbers: string[]
+    profileLinkedPrisonerNumbers: string[]
+    profilePictureDisplayedPrisonerNumbers: string[]
+    licenceLinkedPrisonerNumbers: string[]
+    prisonerInformationDisplayed: string[]
+  }
+}
+
 export default class AuditService {
   constructor(private readonly enabled: boolean) {}
 
@@ -40,6 +79,46 @@ export default class AuditService {
         subjectId: prisonId,
         subjectType: 'CASELOAD_ID',
         correlationId: requestId,
+        service: 'hmpps-digital-prison-services',
+      })
+    }
+  }
+
+  public async auditPrisonerSearch({
+    username,
+    requestId,
+    searchDetails,
+  }: {
+    username: string
+    requestId: string
+    searchDetails: PrisonerSearchDetails
+  }) {
+    if (this.enabled) {
+      await auditService.sendAuditMessage({
+        action: 'PRISONER_SEARCH',
+        who: username,
+        correlationId: requestId,
+        details: JSON.stringify(searchDetails),
+        service: 'hmpps-digital-prison-services',
+      })
+    }
+  }
+
+  public async auditGlobalSearch({
+    username,
+    requestId,
+    searchDetails,
+  }: {
+    username: string
+    requestId: string
+    searchDetails: GlobalSearchDetails
+  }) {
+    if (this.enabled) {
+      await auditService.sendAuditMessage({
+        action: 'GLOBAL_SEARCH',
+        who: username,
+        correlationId: requestId,
+        details: JSON.stringify(searchDetails),
         service: 'hmpps-digital-prison-services',
       })
     }
