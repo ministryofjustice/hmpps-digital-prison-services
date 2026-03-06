38790f80d2931a2ea5f566b39a5f2d00a453b15a
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2026-03-06 15:52:36 +0000

    Add a caseload switcher (no int tests yet)

---
+++ server/controllers/changeCaseloadController.test.ts
@@ -0,0 +1,146 @@
+import type { Request, Response } from 'express'
+import logger from '../../logger'
+import ChangeCaseloadController from './changeCaseloadController'
+import { UserService } from '../services'
+import { CaseLoad } from '../data/interfaces/caseLoad'
+
+jest.mock('../../logger')
+
+describe('ChangeCaseloadController', () => {
+  let controller: ChangeCaseloadController
+  let userService: UserService
+
+  const kirkham = { caseLoadId: 'KMI', description: 'Kirkham' } as CaseLoad
+  const moorland = { caseLoadId: 'MDI', description: 'Moorland' } as CaseLoad
+  const styal = { caseLoadId: 'STI', description: 'Styal' } as CaseLoad
+  const caseloads = [kirkham, moorland]
+
+  const renderedOptions = caseloads.map(caseload => ({
+    value: caseload.caseLoadId,
+    text: caseload.description,
+  }))
+
+  beforeEach(() => {
+    userService = {
+      setActiveCaseload: jest.fn(),
+    } as unknown as UserService
+
+    controller = new ChangeCaseloadController(userService)
+  })
+
+  describe('get', () => {
+    it('Renders changeCaseload page with options and backUrl', async () => {
+      const expectedBackUrl = 'http://localhost/whereTheyCameFrom?queries=values'
+      const req = { headers: { referer: expectedBackUrl } } as unknown as Request
+      const res = {
+        locals: {
+          user: {
+            caseLoads: caseloads,
+          },
+        },
+        render: jest.fn(),
+      } as unknown as Response
+
+      await controller.get()(req, res, jest.fn())
+
+      expect(res.render).toHaveBeenCalledWith(
+        'pages/changeCaseload/changeCaseload',
+        expect.objectContaining({
+          title: 'Change your location',
+          options: renderedOptions,
+          backUrl: expectedBackUrl,
+        }),
+      )
+    })
+
+    it('Does not set backUrl if referer is the change-caseload page', async () => {
+      const req = { headers: { referer: 'http://localhost/change-caseload' } } as unknown as Request
+      const res = {
+        locals: {
+          user: {
+            caseLoads: caseloads,
+          },
+        },
+        render: jest.fn(),
+      } as unknown as Response
+
+      await controller.get()(req, res, jest.fn())
+
+      expect(res.render).toHaveBeenCalledWith(
+        'pages/changeCaseload/changeCaseload',
+        expect.objectContaining({
+          backUrl: undefined,
+        }),
+      )
+    })
+
+    it('Redirects to / if user has only one caseload', async () => {
+      const req = {} as unknown as Request
+      const res = {
+        locals: { user: { caseLoads: [kirkham] } },
+        redirect: jest.fn(),
+      } as unknown as Response
+
+      await controller.get()(req, res, jest.fn())
+
+      expect(res.redirect).toHaveBeenCalledWith('/')
+    })
+  })
+
+  describe('post', () => {
+    it('Sets active caseload and redirects to /', async () => {
+      const req = { body: { caseLoadId: moorland.caseLoadId }, originalUrl: '/change-caseload' } as unknown as Request
+      const res = {
+        locals: { user: { token: 'token', caseLoads: caseloads } },
+        redirect: jest.fn(),
+      } as unknown as Response
+
+      await controller.post()(req, res, jest.fn())
+
+      expect(userService.setActiveCaseload).toHaveBeenCalledWith('token', {
+        caseLoadId: moorland.caseLoadId,
+        description: moorland.description,
+      })
+      expect(res.redirect).toHaveBeenCalledWith('/')
+    })
+
+    it('Logs error and renders error page if caseLoadId is missing', async () => {
+      const req = { body: {}, originalUrl: 'http://localhost/whereTheyCameFrom' } as unknown as Request
+      const res = {
+        locals: { user: { token: 'token', caseLoads: caseloads } },
+        status: jest.fn().mockReturnThis(),
+        render: jest.fn(),
+      } as unknown as Response
+
+      await controller.post()(req, res, jest.fn())
+
+      expect(logger.error).toHaveBeenCalledWith(
+        'http://localhost/whereTheyCameFrom',
+        'Caseload ID is missing, not assigned, or does not exist',
+      )
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.render).toHaveBeenCalledWith('error.njk', { url: '/change-caseload' })
+    })
+
+    it('Logs error and renders error page if user somehow tries to set a caseload they are not assigned to', async () => {
+      const req = {
+        body: { caseLoadId: styal.caseLoadId },
+        originalUrl: 'http://localhost/whereTheyCameFrom',
+      } as unknown as Request
+      const res = {
+        locals: { user: { token: 'token', caseLoads: caseloads } },
+        status: jest.fn().mockReturnThis(),
+        render: jest.fn(),
+      } as unknown as Response
+
+      await controller.post()(req, res, jest.fn())
+
+      expect(logger.error).toHaveBeenCalledWith(
+        'http://localhost/whereTheyCameFrom',
+        'Caseload ID is missing, not assigned, or does not exist',
+      )
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.render).toHaveBeenCalledWith('error.njk', { url: '/change-caseload' })
+    })
+  })
+})
---
+++ server/controllers/changeCaseloadController.ts
@@ -0,0 +1,61 @@
+import { Request, RequestHandler, Response } from 'express'
+import logger from '../../logger'
+import { UserService } from '../services'
+
+export default class ChangeCaseloadController {
+  constructor(private readonly userService: UserService) {}
+
+  public get(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const {
+        user: { caseLoads },
+      } = res.locals
+
+      if (caseLoads.length <= 1) {
+        return res.redirect('/')
+      }
+
+      const options = caseLoads.map(caseload => ({
+        value: caseload.caseLoadId,
+        text: caseload.description,
+      }))
+
+      let backUrl: string
+
+      if (req.headers.referer) {
+        const referer = new URL(req.headers.referer)
+        if (!referer.pathname.match(/\/change-caseload\/?/)) {
+          backUrl = req.headers.referer
+        }
+      }
+
+      return res.render('pages/changeCaseload/changeCaseload', {
+        title: 'Change your location',
+        options,
+        backUrl,
+      })
+    }
+  }
+
+  public post(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { caseLoadId }: { caseLoadId: string } = req.body
+      const {
+        user: { token, caseLoads },
+      } = res.locals
+
+      const caseloadToSet = caseLoads.find(caseload => caseload.caseLoadId === caseLoadId)
+
+      if (!req.body.caseLoadId || !caseloadToSet) {
+        res.status(500)
+        logger.error(req.originalUrl, 'Caseload ID is missing, not assigned, or does not exist')
+        return res.render('error.njk', {
+          url: '/change-caseload',
+        })
+      }
+
+      await this.userService.setActiveCaseload(token, caseloadToSet)
+      return res.redirect('/')
+    }
+  }
+}
