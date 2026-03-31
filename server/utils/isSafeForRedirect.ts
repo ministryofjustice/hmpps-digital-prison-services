78624494372a49c79bed9dd5d1db2c746acd4a9a
Igor <igor.ushkarev1@justice.gov.uk>
2026-03-31 16:47:18 +0100

    Add utility to check that a URL is “safe” to redirect to: is the same host as configured in INGRESS_URL or ends in .service.justice.gov.uk. https is required in production and for external hosts.

---
+++ server/utils/isSafeForRedirect.ts
@@ -0,0 +1,28 @@
+import config from '../config'
+
+const safeHostSuffixes = ['.service.justice.gov.uk']
+
+/**
+ * Whether a redirect URL is to a safe/trusted host; must be absolute.
+ * Configured INGRESS_URL and hosts ending in .service.justice.gov.uk are considered safe.
+ * https is required in production and for external hosts.
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function isSafeForRedirect(url: string): boolean {
+  const { domain, production } = config
+  const { hostname: homepageHostname } = new URL(domain)
+  if (typeof url !== 'string') {
+    return false
+  }
+  try {
+    const { hostname: redirectHostname, protocol } = new URL(url)
+    const redirectsToSecure = protocol === 'https:'
+    return (
+      (redirectHostname === homepageHostname && (redirectsToSecure || !production)) ||
+      (safeHostSuffixes.some(suffix => redirectHostname.endsWith(suffix)) && redirectsToSecure)
+    )
+  } catch {
+    // invalid url
+    return false
+  }
+}
---
+++ server/utils/isSafeRedirect.test.ts
@@ -0,0 +1,80 @@
+import { isSafeForRedirect } from './isSafeForRedirect'
+import config from '../config'
+
+jest.mock('../config')
+
+describe('isSafeForRedirect()', () => {
+  beforeEach(() => {
+    config.domain = 'https://dps.prison.service.justice.gov.uk'
+    config.production = true
+  })
+
+  it.each([null, undefined, '', '/', 'link?page=2', 'https://'])(
+    'should not consider %j to be safe because it is not a valid absolute URL',
+    url => {
+      expect(isSafeForRedirect(url)).toBe(false)
+    },
+  )
+
+  it('should throw an error if configured INGRESS_URL is invalid', () => {
+    config.domain = 'https://'
+    expect(() => isSafeForRedirect('https://dps.prison.service.justice.gov.uk')).toThrow()
+  })
+
+  it.each([
+    'https://dps.prison.service.justice.gov.uk',
+    'https://dps.prison.service.justice.gov.uk/',
+    'https://dps.prison.service.justice.gov.uk/whats-new?search=2025',
+  ])('should consider %j to be safe because it has the same host', url => {
+    expect(isSafeForRedirect(url)).toBe(true)
+  })
+
+  it.each([
+    'https://dps-dev.prison.service.justice.gov.uk/whats-new',
+    'https://prisoner.digital.prison.service.justice.gov.uk/prisoner/A1111AA',
+    'https://incident-reporting.hmpps.service.justice.gov.uk/',
+  ])('should consider %j to be safe because *.service.justice.gov.uk hosts are trusted', url => {
+    expect(isSafeForRedirect(url)).toBe(true)
+  })
+
+  it.each(['https://www.gov.uk/', 'https://localhost/'])(
+    'should not consider %j to be safe because the host is not explicitly trusted',
+    url => {
+      expect(isSafeForRedirect(url)).toBe(false)
+    },
+  )
+
+  describe('in production (ie. dev, preprod or prod)', () => {
+    it.each([
+      'http://dps.prison.service.justice.gov.uk/whats-new?search=2025',
+      'http://dps-dev.prison.service.justice.gov.uk/whats-new',
+      'http://prisoner.digital.prison.service.justice.gov.uk/prisoner/A1111AA',
+      'http://incident-reporting.hmpps.service.justice.gov.uk/',
+    ])('should not consider %j to be safe because it is insecure', url => {
+      expect(isSafeForRedirect(url)).toBe(false)
+    })
+  })
+
+  describe('locally', () => {
+    beforeEach(() => {
+      config.domain = 'http://localhost:3000'
+      config.production = false
+    })
+
+    it.each(['http://localhost/', 'http://localhost:3000/'])(
+      'should consider %j to be safe despite not being secure',
+      url => {
+        expect(isSafeForRedirect(url)).toBe(true)
+      },
+    )
+
+    it.each([
+      'http://dps.prison.service.justice.gov.uk/whats-new?search=2025',
+      'http://dps-dev.prison.service.justice.gov.uk/whats-new',
+      'http://prisoner.digital.prison.service.justice.gov.uk/prisoner/A1111AA',
+      'http://incident-reporting.hmpps.service.justice.gov.uk/',
+    ])('should not consider %j to be safe because it is external and insecure', url => {
+      expect(isSafeForRedirect(url)).toBe(false)
+    })
+  })
+})
